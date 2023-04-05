d74d2561088a145fc9c544c13c73c064b0193cc0
Jackson Abiusi <jackson.abiusi@gmail.com>
2023-04-04 20:52:51 -0700

    push

--- src/fifteenpuzzle/StateGraph.java
+++ src/fifteenpuzzle/StateGraph.java
@@ -1,57 +1,16 @@
 package fifteenpuzzle;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class StateGraph {
-  private ArrayList<Vertex> vertices;
+  private Map<Vertex, List<Vertex>> adjVertices;
 
 
-  public StateGraph(boolean inputIsWeighted, boolean inputIsDirected) {
-    this.vertices = new ArrayList<Vertex>();
-
-  }
-
-  public Vertex addVertex(FifteenPuzzle data) {
-    Vertex newVertex = new Vertex(data);
-    this.vertices.add(newVertex);
-    return newVertex;
-  }
-
-  public void addEdge(Vertex vertex1, Vertex vertex2, Integer weight) {
-    
-    vertex1.addEdge(vertex2, weight);
-
-  }
-
-  public void removeEdge(Vertex vertex1, Vertex vertex2) {
-    vertex1.removeEdge(vertex2);
-  }
-
-  public void removeVertex(Vertex vertex) {
-    this.vertices.remove(vertex);
-  }
-
-  public ArrayList<Vertex> getVertices() {
-    return this.vertices;
-  }
-
-
-
-
-  public Vertex getVertexByValue(FifteenPuzzle value) {
-    for(Vertex v: this.vertices) { 
-      if (v.getData() == value) {
-        return v;
-      }
-    }
-
-    return null;
-  }
-	
-  public void print() {
-    for(Vertex v: this.vertices) {
-     
-    }
-  }
 	
 }
